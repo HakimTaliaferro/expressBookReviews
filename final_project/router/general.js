804f1851aabf36c6d6916c737fd611b7bd734971
HakimTaliaferro <taliaferro.hakim@gmail.com>
2025-04-06 06:22:36 -0400

    Push more

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,6 +20,16 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
+  const isbn = req.params.isbn;  // Get the ISBN from the request parameters
+  const book = books[isbn];      // Try to find the book by ISBN in the books object
+  
+  if (book) {
+    // If the book is found, return it as a JSON response
+    res.status(200).json(book);
+  } else {
+    // If the book is not found, return a 404 error with a message
+    res.status(404).json({ message: "Book with ISBN " + isbn + " not found" });
+  }
   
   return res.status(300).json({message: "Yet to be implemented"});
  });
@@ -27,6 +37,18 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
+  const author = req.params.author;
+let result = [];
+for (let key in books) {
+  if (books[key].author.toLowerCase() === author.toLowerCase()) {
+    result.push(books[key]);
+  }
+}
+if (result.length > 0) {
+  res.send(JSON.stringify(result, null, 4));
+} else {
+  res.status(404).json({ message: "No books found for this author" });
+}
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
